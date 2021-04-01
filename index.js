5d349344c1886ced43a05b24d967e6f854a7231b
Matthew Riegg <matthew.d.riegg@gmail.com>
2021-03-31 18:30:56 -0700

    Deliverables

--- index.js
+++ index.js
@@ -1,1 +1,6 @@
-// Follow along with the examples here
+function userInfo(firstName, lastName, checkInTime){
+return `Welcome, ${firstName}, ${lastName}, to the Belagio. You checked in at ${checkInTime} o clock.`
+}
+
+console.log(userInfo('Moses', 'Malone', 'six'));
+
